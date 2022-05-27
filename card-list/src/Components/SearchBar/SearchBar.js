e05e82628ae7bbcbb3eeaf721e440aeacdda1510
Alvaro R Martin <exagonsoft@gmail.com>
2022-05-27 13:15:12 +0000

    feat: Adding Components
    Adding components to the React App
    
    SearchBar.js
    BarContainer.js

---
+++ card-list/src/Components/SearchBar/SearchBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { BarContainer } from './Components/BarContainer'
+
+const SearchBar = () => {
+  return (
+    <>
+      <BarContainer />
+    </>
+  )
+}
+
+export default SearchBar
