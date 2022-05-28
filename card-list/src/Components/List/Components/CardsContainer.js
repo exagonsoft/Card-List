b3f5e60946fef0af04f564959a8caedfc6c321c2
Alvaro R Martin <exagonsoft@gmail.com>
2022-05-28 23:41:27 +0000

    feat: Adding Components
    Adding components to React App
    
    Adding components
    CardsContainer.js
    CardsHQContainer.js
    CardsCharContainer.js
    CardsTechContainer.js

---
+++ card-list/src/Components/List/Components/CardsContainer.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const CardsContainer = styled.div`
+  width: 90%;
+  height: 100%;
+  margin-left: 5%;
+  padding: 20px;
+  border: 2px solid #cc;
+  border-radius: 10px;
+`
