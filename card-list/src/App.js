585a6c972516e7389835070e510b7b6856034f71
Alvaro R Martin <exagonsoft@gmail.com>
2022-05-28 23:11:54 +0000

    upw: Adding Functionality
    Adding new functionality
    
    Adding code to components
    App.js
    SearchBar.js
    
    In order to create the search by Name and Filters functionality

--- card-list/src/App.js
+++ card-list/src/App.js
@@ -5,6 +5,7 @@ import SearchBar from './Components/SearchBar/SearchBar';
 
 function App() {
   const Filters = {
+    Name: "",
     HQ: false,
     Character: false,
     Technology: false,
@@ -24,14 +25,18 @@ function App() {
   function OnFiltersClick(filtersData){
     setshowFilters(!showFilters);
     setFilters(filtersData);
-    console.log(filtersData);
+    console.log(filters);
   }
-  function OnFilterSelected(){
-
+  function OnNameFilterChange(sender, field){
+    setFilters({
+      ...filters,
+      [field]: sender.target.value,
+    });
+    console.log(filters);
   }
   return (
     <>
-      <SearchBar showFilters={showFilters} OnFiltersClick={OnFiltersClick} />
+      <SearchBar showFilters={showFilters} OnNameFilterChange={OnNameFilterChange} OnFiltersClick={OnFiltersClick} />
       <List Filters={filters}/>
     </>
   );
--- card-list/src/Components/SearchBar/SearchBar.js
+++ card-list/src/Components/SearchBar/SearchBar.js
@@ -1,23 +1,108 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { BarContainer } from './Components/BarContainer'
 import { FilterIcon } from './Components/FilterIcon'
 import { FiltersButton } from './Components/FiltersButton'
 import { FiltersContainer } from './Components/FiltersContainer'
 import AccountFilterIcon from '../../Pictures/AccountFilterIcon.png'
 import { SearchByName } from './Components/SearchByName'
+import { FiltersContainerBorder } from './Components/FiltersContainerBorder'
+import { FiltersContainerItemList } from './Components/FiltersContainerItemsList'
+import { FiltersContainerTitle } from './Components/FiltersContainerTitle'
+import { FiltersContainerSubTitle } from './Components/FiltersContainerSubTitle'
+import { FiltersContainerCheckBox } from './Components/FilterContainerCheckBox'
+import { FiltersContainerCheckBoxWrapper } from './FiltersContainerCheckBoxWrapper'
+import { FiltersContainerWrapper } from './Components/FiltersContainerWrapper'
 
-const SearchBar = () => {
+const SearchBar = (props) => {
+  const Filters = {
+    Name: "",
+    HQ: false,
+    Character: false,
+    Technology: false,
+    Awaken: false,
+    Entropy: false,
+    Inhuman: false,
+    Owner: false,
+    Undeviant: false,
+    Legendary: false,
+    Rare: false,
+    UnCommon: false,
+    Common: false,
+  };
+  const [filters, setFilters] = useState(Filters);
+  function UpdateFilters(sender, field){
+    let data = null;
+
+    if (sender.target.checked) {
+      data = true;
+    } else {
+      data = false;
+    }
+    setFilters({
+      ...filters,
+      [field]: data,
+    });
+  }
   return (
     <>
       <BarContainer >
-        <FiltersButton>
+        <FiltersContainerWrapper>
+        <FiltersButton onClick={() => {
+          let filterData = filters;
+          props.OnFiltersClick(filterData);
+        }}>
           <FilterIcon src={AccountFilterIcon} />
-          Apply Filters
+          {props.showFilters ? ('Apply') : ('Filters')}
         </FiltersButton>
-        <FiltersContainer>
-          
+        <FiltersContainer Showed={props.showFilters}>
+          <FiltersContainerBorder />
+          <FiltersContainerItemList>
+            <FiltersContainerTitle>FILTERS</FiltersContainerTitle>
+            <FiltersContainerSubTitle>Card Type</FiltersContainerSubTitle>
+            <FiltersContainerCheckBoxWrapper>
+              <FiltersContainerCheckBox id="filterTypeHQ" onChange={(sender) => UpdateFilters(sender, "HQ")} type="checkbox"></FiltersContainerCheckBox>HQ
+            </FiltersContainerCheckBoxWrapper>
+            <FiltersContainerCheckBoxWrapper>
+            <FiltersContainerCheckBox id="filterTypeCHR" onChange={(sender) => UpdateFilters(sender, "Character")} type="checkbox"></FiltersContainerCheckBox>Character
+            </FiltersContainerCheckBoxWrapper>
+            <FiltersContainerCheckBoxWrapper>
+            <FiltersContainerCheckBox id="filterTypeTECH" onChange={(sender) => UpdateFilters(sender, "Technology")} type="checkbox"></FiltersContainerCheckBox>Technology
+            </FiltersContainerCheckBoxWrapper>
+            <FiltersContainerSubTitle>Factions</FiltersContainerSubTitle>
+            <FiltersContainerCheckBoxWrapper>
+              <FiltersContainerCheckBox id="filterFactionAwaken" onChange={(sender) => UpdateFilters(sender, "Awaken")} type="checkbox"></FiltersContainerCheckBox>Awaken
+            </FiltersContainerCheckBoxWrapper>
+            <FiltersContainerCheckBoxWrapper>
+            <FiltersContainerCheckBox id="filterFactionEntropy" onChange={(sender) => UpdateFilters(sender, "Entropy")} type="checkbox"></FiltersContainerCheckBox>Entropy
+            </FiltersContainerCheckBoxWrapper>
+            <FiltersContainerCheckBoxWrapper>
+            <FiltersContainerCheckBox id="filterFactionInhuman" onChange={(sender) => UpdateFilters(sender, "Inhuman")} type="checkbox"></FiltersContainerCheckBox>Inhuman
+            </FiltersContainerCheckBoxWrapper>
+            <FiltersContainerCheckBoxWrapper>
+            <FiltersContainerCheckBox id="filterFactionOwner" onChange={(sender) => UpdateFilters(sender, "Owner")} type="checkbox"></FiltersContainerCheckBox>Owner
+            </FiltersContainerCheckBoxWrapper>
+            <FiltersContainerCheckBoxWrapper>
+            <FiltersContainerCheckBox id="filterFactionUndeviant" onChange={(sender) => UpdateFilters(sender, "Undeviant")} type="checkbox"></FiltersContainerCheckBox>Undeviant
+            </FiltersContainerCheckBoxWrapper>
+            <FiltersContainerSubTitle>Rarity</FiltersContainerSubTitle>
+            <FiltersContainerCheckBoxWrapper>
+              <FiltersContainerCheckBox id="filterRarityLeg" onChange={(sender) => UpdateFilters(sender, "Legendary")} type="checkbox"></FiltersContainerCheckBox>Legendary
+            </FiltersContainerCheckBoxWrapper>
+            <FiltersContainerCheckBoxWrapper>
+            <FiltersContainerCheckBox id="filterRarityRare" onChange={(sender) => UpdateFilters(sender, "Rare")} type="checkbox"></FiltersContainerCheckBox>Rare
+            </FiltersContainerCheckBoxWrapper>
+            <FiltersContainerCheckBoxWrapper>
+            <FiltersContainerCheckBox id="filterRarityUnCom" onChange={(sender) => UpdateFilters(sender, "UnCommon")} type="checkbox"></FiltersContainerCheckBox>UnCommon
+            </FiltersContainerCheckBoxWrapper>
+            <FiltersContainerCheckBoxWrapper>
+            <FiltersContainerCheckBox id="filterRarityCommon" onChange={(sender) => UpdateFilters(sender, "Common")} type="checkbox"></FiltersContainerCheckBox>Common
+            </FiltersContainerCheckBoxWrapper>
+          </FiltersContainerItemList>
         </FiltersContainer>
-        <SearchByName placeholder="Search By Name"/>
+        </FiltersContainerWrapper>
+        <SearchByName placeholder="Search By Name" onChange={(sender) => {
+          props.OnNameFilterChange(sender, "Name");
+        }}/>
       </BarContainer>
     </>
   )
