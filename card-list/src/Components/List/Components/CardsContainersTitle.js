8b2da0df055caeea237d7fe44d506c73a4754251
Alvaro R Martin <exagonsoft@gmail.com>
2022-05-29 00:33:31 +0000

    feat: Adding Components
    Adding new components to React App
    
    Creating new components
    CardsContainerTitle.js
    CardsItemContainer.js

---
+++ card-list/src/Components/List/Components/CardsContainersTitle.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+
+export const CardsContainersTitle = styled.h3`
+  color: #FFFFFF;
+  margin: 0 0 10px 0;
+`
---
+++ card-list/src/Components/List/Components/CardsItemContainer.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+
+export const CardsItemsContainer = styled.div`
+   width: 100%;
+   display: flex;
+`
